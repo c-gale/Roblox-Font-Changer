6d7b82b5ed957dd51ba4be41c31569bbfc8202ac
OpticalDevOGS <91436354+OpticalDevOGS@users.noreply.github.com>
2024-08-26 23:03:24 -0600

    cursor only

--- main.py
+++ main.py
@@ -10,26 +10,12 @@ def get_file_extensions(directory):
             _, extension = os.path.splitext(filename)
             extensions.add(extension)
     return extensions
-
-def has_font_extension(file_path):
-    _, extension = os.path.splitext(file_path)
-    return extension.lower() in ['.ttf', '.otf']
-
-def is_file_blacklisted(file_path, blacklist):
-    return os.path.basename(file_path) in blacklist
     
 def get_roblox_directory(path):
     for v in os.listdir(path):
         if os.path.exists(path+"\\"+v+"\\content\\fonts\\"):
             if os.path.exists(path+"\\"+v+"\\RobloxPlayer.exe") or os.path.exists(path+"\\"+v+"\\RobloxPlayerBeta.exe"):
                 return path+"\\"+v
-            
-
-def hasFileInDirectory(directory):
-    for t in os.listdir(directory):
-        if has_font_extension(t):
-            return True
-    return False
 
 def copy_file(source_file, destination_directory):
     shutil.copy(source_file, destination_directory)
@@ -52,41 +38,26 @@ class main():
         self.robloxVersionPath = get_roblox_directory(self.config["robloxVersionsPath"])
         print(f"GOT VERSION: ({os.path.basename(self.robloxVersionPath)})")
         
-        print("INITIALIZING FONTS...")
+        print("changing cursor...")
         time.sleep(1)
         
-        self.fonts = {}
-        
-        self.getAllFontsInRBLX(self.robloxVersionPath + r"\content\fonts")
-
-        if hasFileInDirectory("./font/"):
-            print("USING FONT PATH "+str(__file__).rstrip(str(__file__)[-(len(str(__name__))-1):])+r"font\font.ttf")
-            self.replacementFont = str(__file__).rstrip(str(__file__)[-(len(str(__name__))-1):])+r"font\font.ttf"
+        try:
+            if os.path.exists("./cursor/cursor.png"):
+                print("USING CURSOR: ./cursor/cursor.png")
+                
+                print("ArrowCursor.png")
+                delete_file(self.robloxVersionPath+"/content/textures/Cursors/KeyboardMouse/ArrowCursor.png")
+                print("ArrowFarCursor.png")
+                delete_file(self.robloxVersionPath+"/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png")
+                time.sleep(1)
+                copy_file("./cursor/cursor.png", self.robloxVersionPath+"/content/textures/Cursors/KeyboardMouse/ArrowCursor.png")
+                copy_file("./cursor/cursor.png", self.robloxVersionPath+"/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png")
+                print("REPLACED CURSORS WITH cursor.png")
+                
+                print("changes complete!")
+                time.sleep(2)
             
-        print("REMOVING ALL OLD FONTS")
-        for t in self.fonts:
-            print("|-REMOVING " + self.fonts[t] + " FROM DIRECTORY " + self.robloxVersionPath+ r"\content\fonts")
-            if os.path.exists(self.robloxVersionPath + "\\content\\fonts\\" + self.fonts[t]):
-                delete_file(self.robloxVersionPath+ '\\content\\fonts\\' + self.fonts[t])
-
-        for t in self.fonts:
-            copy_file(self.replacementFont, self.robloxVersionPath+"/content/fonts/")
-            if os.path.exists(self.robloxVersionPath+"/content/fonts/font.ttf"):
-                newPath = self.robloxVersionPath+"/content/fonts/font.ttf"
-                rename_file(newPath, self.fonts[t])
-            else:
-                print("WAHT DA HELL WEN WRONG?")
-                return
-            
-        print("everything should be that font now!")
-
-    def getAllFontsInRBLX(self, path):
-        if os.path.exists(path):
-            for index, t in enumerate(os.listdir(path)):
-                print(t)
-                if has_font_extension(t):
-                    if not is_file_blacklisted(t, self.config["fontBlacklist"]):
-                        self.fonts[index] = t
-                        print(path+t)
+        except Exception as e:
+            print(f"An Error Occured when changing cursor: {e}")
            
 app = main()
